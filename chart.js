d71353406b6e470fab9f52f2280c4d252a0d8b2b
Julia Teitelbaum <julialt@gmail.com>
2013-03-27 02:19:21 -0400

    added kinetic and really brief css. mostly worked on drawing of logarithmic chart, but for some reason javascript is refusing to loop in the drawXAxis function.

--- chart.js
+++ chart.js
@@ -3,27 +3,45 @@
 */
 
 function Chart() {
-	this.height = 1000px; //temp
-	this.width = 1400px; //temp
 	this.numberOfDays = 140;
 	this.minCountPerMinuteExponent = -3;
 	this.maxCountPerMinuteExponent = 3;
-	this.chartDiv = $('chart'); 
+	this.decades = Math.abs(this.minCountPerMinuteExponent) + Math.abs(this.maxCountPerMinuteExponent);
+	this.chartElement = $('#chart'); 
 }
+
+/* Setup a new Kinetic.js Stage (which can contain multiple HTML5 canvases) */
+Chart.prototype.init = function() {
+	this.stage = new Kinetic.Stage({
+		container: 'chart',
+		height: this.chartElement.height(),
+		width: this.chartElement.width()
+	})
+};
 
 Chart.prototype.drawXAxis = function() {
-	// solve equation where value equals two to nine, then multiply by base of the decade
+	// for each decade, draw the lines within that decade on the log scale
+	var i;
+	for (i = 0; i++; i < 6) {
+		var decadeBaseValue = Math.pow(10, this.minCountPerMinuteExponent + i);
+		var decadeHighValue = Math.pow(10, this.minCountPerMinuteExponent + i + 1);
+		// draw lines for the decade high value and the decade low value
+		// [call line drawing function here]
+		console.log('decade base value for decade ' + i + ' is ' + decadeBaseValue);
+		console.log('decade high value for decade ' + i + ' is ' + decadeHighValue);
+
+		// get y positions for and draw lines for values in between the high and the low
+		var yPosition;
+		for (var j = 2; j++; j < 10) {
+			// solve equation where value equals two to nine, then multiply by base of the decade
+			yPosition = valueToYPosition(decadeHighValue, decadeBaseValue, j, decadeBaseValue);
+			console.log('y Position for intermediate line '+ j + ' : ' + yPosition);
+		}
+	}
+	return 'done';
 }
 
-// takes a point on the chart and converts it to a semantic numeric value
-Chart.prototype.pointToValue = function(hi, lo, yPosition, decadeBaseValue) {
-	var decadeRange = hi - lo;
-	var percentAwayFromVBase = (y - lo)/decadeRange;
-	var value = decadeBaseValue - pow(10, percentAwayFromVBase);
-	return value;
-}
-
-// takes a value and coverts it to a point for its y position on the chart
+// takes a value and coverts it to a y position on the chart
 Chart.prototype.valueToYPosition = function(hi, lo, value, decadeBaseValue) {
 	var decadeRange = hi-lo;
 	var y = 10 + decadeRange * (log10(value/decadeBaseValue));
@@ -48,7 +66,15 @@ Chart.prototype.readPoint = function() {
 	// multiply that by base value in the decade
 }
 
-// maybe only draw decades
+// takes a point on the chart and converts it to a semantic numeric value
+Chart.prototype.pointToValue = function(hi, lo, yPosition, decadeBaseValue) {
+	var decadeRange = hi - lo;
+	var percentAwayFromVBase = (y - lo)/decadeRange;
+	var value = decadeBaseValue - pow(10, percentAwayFromVBase);
+	return value;
+}
+
+// note: for sooming maybe only draw decades
 
 // just a helper to do log10 of numbers (javascript doesn't let you specify the base) of a log normally
 function log10(value) {
