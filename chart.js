9177c7c784425aa97890ad895e3ec04e874e1b01
Julia Teitelbaum <julialt@gmail.com>
2013-03-26 23:41:46 -0400

    mostly empty files with a blank bit of javascript

---
+++ chart.js
@@ -0,0 +1,21 @@
+/* Chart Class 
+ * ========================================================================= *
+*/
+
+function Chart() {
+	this.numberOfDays = 140;
+	this.minCountPerMinuteExponent = 
+	this.chart = $('chart'); // this is only theoretical. this element does not exist in the html page now
+}
+
+Chart.prototype.drawLines = function() {
+
+}
+
+Chart.prototype.drawHistoricalData = function() {
+
+}
+
+Chart.prototype.readPoint = function() {
+	// take the y value, it's the exponent
+}
